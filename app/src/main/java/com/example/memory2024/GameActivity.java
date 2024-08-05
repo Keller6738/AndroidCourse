33c04431283120fbb8ecbb0b8f1e14b8d33961f3
Keller6738 <itaykeller12345@gmail.com>
2024-08-05 13:32:34 +0300

    added programming for the buttons and the board

--- app/src/main/java/com/example/memory2024/GameActivity.java
+++ app/src/main/java/com/example/memory2024/GameActivity.java
@@ -3,9 +3,10 @@ package com.example.memory2024;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,8 +17,21 @@ import androidx.core.view.WindowInsetsCompat;
 public class GameActivity extends AppCompatActivity implements View.OnClickListener {
     private TextView tvName1, tvName2;
     private ImageButton btnBack;
+    private Button btnRestart, btnShowCards;
 
-    private ImageView[][] cards;
+    private ImageButton[][] cards;
+    private int[] pictures = new int[]{
+            R.drawable.car0,
+            R.drawable.car1,
+            R.drawable.car2,
+            R.drawable.car3,
+            R.drawable.car4,
+            R.drawable.car5,
+            R.drawable.car6,
+            R.drawable.car7,
+            R.drawable.car8,
+            R.drawable.car9,
+    };
 
     private GameManager gameManager;
 
@@ -33,12 +47,18 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         });
 
         gameManager = new GameManager(this);
+        Toast.makeText(getApplicationContext(), gameManager.printMat(), Toast.LENGTH_LONG).show();
 
         tvName1 = findViewById(R.id.tvName1);
         tvName2 = findViewById(R.id.tvName2);
 
         btnBack = findViewById(R.id.imgBack);
         btnBack.setOnClickListener(this);
+
+        btnRestart = findViewById(R.id.btnRestart);
+        btnRestart.setOnClickListener(this);
+        btnShowCards = findViewById(R.id.btnShowCards);
+        btnShowCards.setOnClickListener(this);
 
         Intent intent = getIntent();
         Bundle extras;
@@ -53,7 +73,30 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         tvName1.setText(player1);
         tvName2.setText(player2);
 
-        this.cards = new ImageView[4][5];
+        cards = new ImageButton[4][5];
+
+        String str;
+        int resId;
+
+        for (int i = 0; i < cards.length; i++) {
+            for (int j = 0; j < cards[i].length; j++) {
+                str = "img" + i + j;
+                resId = getResources().getIdentifier(str, "id", getPackageName());
+                cards[i][j] = findViewById(resId);
+                cards[i][j].setOnClickListener(this);
+            }
+        }
+
+    }
+
+    private void restartBoardView() {
+        int resId;
+
+        for (int i = 0; i < cards.length; i++) {
+            for (int j = 0; j < cards[i].length; j++) {
+                cards[i][j].setImageResource(R.drawable.spongebob);
+            }
+        }
     }
 
     @Override
@@ -62,6 +105,21 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         if (view.getId() == btnBack.getId()) {
             intent = new Intent(this, MainActivity.class);
             startActivity(intent);
+        } else if (view.getId() == btnRestart.getId()) {
+            restartBoardView();
+            gameManager.restartBoard();
+        } else if (view.getId() == btnShowCards.getId()) {
+            Toast.makeText(getApplicationContext(), gameManager.printMat(), Toast.LENGTH_LONG).show();
+        }
+
+        boolean found = false;
+        for (int i = 0; i < cards.length && !found; i++) {
+            for (int j = 0; j < cards[i].length && !found; j++) {
+                if (view.getId() == cards[i][j].getId()) {
+                    found = true;
+                    cards[i][j].setImageResource(pictures[gameManager.getPickedNum(i, j)]);
+                }
+            }
         }
     }
 }
