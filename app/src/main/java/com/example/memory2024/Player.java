8cdc93ab9cfca3cca3a7b70727c68e54e2fb9406
Keller6738 <itaykeller12345@gmail.com>
2024-08-06 09:51:07 +0300

    added Player's fields

--- app/src/main/java/com/example/memory2024/Player.java
+++ app/src/main/java/com/example/memory2024/Player.java
@@ -1,4 +1,15 @@
 package com.example.memory2024;
 
+import java.util.ArrayList;
+
 public class Player {
+    private final String m_name;
+    private int m_score;
+    private ArrayList<Integer> m_cards;
+
+    public Player(String name) {
+        this.m_name = name;
+        this.m_score = 0;
+        this.m_cards = new ArrayList<>();
+    }
 }
